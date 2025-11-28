13b12c71b41ef84b5825f2dc9941ddcdd454567d
Andriy Polishchuk <andrew.polishchuk.a@gmail.com>
2025-11-29 01:17:21 +0200

    CSL-16 Correct the minor mistakes in the files and scripts

--- src/utils/generate_transactions.py
+++ src/utils/generate_transactions.py
@@ -224,7 +224,7 @@ def _collect_rows(
             if s_zr > s:
                 q = (s_zr - s) % curve.n
                 a = s % q if q != 0 else 0
-                if a > 1 and a != s: # Scanning mechanism for test/real blockchain data when total cases = cases count (A + B + C + D)
+                if a < s: # Scanning mechanism for test/real blockchain data. Conditions a < s or a == s
                     total_cases += 1
                     # hidden data analysis
                     s_zk = (z * k_inv) % curve.n
